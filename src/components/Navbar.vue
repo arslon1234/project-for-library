248a697c09c86219fe1149b58306ec871e06f0a1
alimovich <arslon@gamil.com>
2022-05-16 13:16:21 +0500

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="navbars">
+    <div class="nav_top">
+      <span>Toshkent Axborot Texnologiyalar Universiteti</span>
+    </div>
+    <div class="nav_centre">
+      <div class="bottom_left">
+        <p class="tatu">Kutubxona</p>
+      </div>
+      <div class="bottom_right">
+        <div class="input">
+          <input type="text" placeholder="Search this site" />
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div class="select">
+          <select>
+            <option value="en">En</option>
+            <option value="ru">Ru</option>
+            <option value="uz">Uz</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div class="nav_bottom">
+      <ul>
+             <li>
+               <router-link to='/bosh_sahifa' class="router">Bosh sahifa</router-link>
+               </li>
+             <li>Yangiliklar</li>
+             <li>Kutubxona haqida malumot</li>
+             <li>Kitoblar</li>
+             <li>Kutubxonalar</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import en from "@/assets/en.jpg";
+import ru from "@/assets/ru.jpg";
+import uz from "@/assets/uz.jpg";
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.navbars {
+  width: 100%;
+  height: 150px;
+  box-shadow: 0px 1px 3px 5px #ccc;
+}
+.navbars .nav_top {
+  width: 100%;
+  height: 30px;
+  background-color: #b1040e;
+  display: flex;
+  align-items: center;
+}
+.navbars .nav_top span {
+  color: #f9ebec;
+  font-size: 17px;
+  font-family: "Roboto", sans-serif;
+  margin-left: 80px;
+  font-weight: 500;
+  letter-spacing: 0.5px;
+}
+.navbars .nav_centre {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.navbars .nav_centre .bottom_left {
+  margin-left: 80px;
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+}
+.navbars .nav_centre .bottom_left .tatu {
+  font-size: 35px;
+  color: #8c1515;
+  font-family: "Roboto", sans-serif;
+}
+.navbars .nav_centre .bottom_right {
+  margin-right: 80px;
+  display: flex;
+  align-items: center;
+  /* justify-content: space-between; */
+}
+.navbars .nav_centre .bottom_right .input {
+  width: 280px;
+  height: 35px;
+  border: 1px solid #999;
+  display: flex;
+  align-items: center;
+  border-radius: 15px;
+}
+.navbars .nav_centre .bottom_right .input input {
+  width: 240px;
+  outline: none;
+  border: none;
+  border-radius: 15px;
+  padding: 0px 10px;
+  color: #5c5b5b;
+}
+.navbars .nav_centre .bottom_right .input i {
+  font-size: 18px;
+  color: #8c1515;
+}
+.navbars .nav_centre .bottom_right .select {
+  margin-left: 10px;
+}
+.navbars .nav_centre .bottom_right .select select {
+  width: 80px;
+  height: 32px;
+  border-radius: 10px;
+}
+.navbars .nav_bottom {
+  width: 100%;
+  margin-top: 15px;
+}
+.navbars .nav_bottom ul{
+       width: 100%;
+       display: flex;
+       align-items: center;
+       justify-content: center;
+}
+.navbars .nav_bottom ul li{
+       list-style: none;
+       margin: 0px 15px;
+       font-size: 18px;
+       color: #2e2d2d;
+       font-family: 'Roboto', sans-serif;
+       font-weight: 500;
+       cursor: pointer;
+       text-decoration: none;
+}
+.navbars .nav_bottom ul li .router{
+  text-decoration: none;
+    color: #2e2d2d;
+}
+.navbars .nav_bottom ul li .router:hover{
+       color: #B1040E;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar/>
+    <div class="bg_img">
+      <span>“If you are going to get anywhere in life you have to read a lot of books.”</span>
+      <p>Roald Dahl</p>
+    </div>
+    <div class="app">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Navbar from '@/components/Navbar'
 </script>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.home{
+  width: 100%;
+}
+.bg_img{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-image: url('../assets/library.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.bg_img span{
+  font-size: 30px;
+  counter-reset: #fff;
+}
+.app{
+  width: 100%;
+}
+</style>
